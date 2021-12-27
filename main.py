06114a44bfb0e9d7e6575932704206eab267e7a2
blue-hexagon <w0j8uhv5csio@opayq.net>
2021-12-27 19:14:07 +0100

    Version 1.0.0

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-import argparse
-
-from app.helper.descriptor_helper import Descriptors
-from app.view.help import HelpView
-from app.view.winscraper import WinScraper
+from app.winscraper import WinScraper
 
 if __name__ == "__main__":
-    if CLI := True:
-
-        parser = argparse.ArgumentParser("Collect a plethora of information about this device.")
-        for descriptor in Descriptors.all_descriptors:
-            parser.add_argument(descriptor.cmd_arg, help=descriptor.description, action="store_true")
-        for category in Descriptors.all_categories:
-            parser.add_argument(category.cmd_arg, help=category.description, action="store_true")
-        args = parser.parse_args()
-        WinScraper(**vars(args)).print()
-    else:
-        sysinfo = WinScraper()
-        sysinfo.print()
-        HelpView.display()
+    """Run ./main.py -h for information about commandline usage"""
+    """ Run WinScraper() for information about library usage """
+    WinScraper(ssid=True)
